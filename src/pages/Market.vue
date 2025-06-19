3032c11f414a2dc757d562c1e0c029d4ae5dbbbd
novice-is-me <reginajalandoni13@gmail.com>
2025-06-19 10:26:34 +0800

    Fixing responsiveness

--- src/pages/Market.vue
+++ src/pages/Market.vue
@@ -5,7 +5,7 @@ import CryptoCard from "@/components/CryptoCard.vue";
 
 <template>
   <div class="pt-10 space-y-14">
-    <div class="space-y-4 font-[Inter]">
+    <div class="space-y-4 font-[Inter] text-center md:text-start">
       <h1 class="crypto-text-gradient text-4xl">Cryptocurrency Market</h1>
       <p class="text-subtext">
         Real-time prices and market data for the top cryptocurrencies
@@ -14,11 +14,11 @@ import CryptoCard from "@/components/CryptoCard.vue";
 
     <!-- Search -->
     <div
-      class="glass-effect p-6 rounded-lg flex items-center gap-x-4 justify-between"
+      class="glass-effect p-6 rounded-lg flex items-center gap-x-4 justify-between flex-col md:flex-row"
     >
       <!-- Search Input -->
       <div
-        class="flex items-center rounded-lg w-1/2"
+        class="flex items-center rounded-lg w-full md:w-1/2"
         style="border: 1px solid rgba(255, 255, 255, 0.1)"
       >
         <i class="fa-solid fa-magnifying-glass text-subtext p-2"></i>
