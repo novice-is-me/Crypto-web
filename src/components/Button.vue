122e21f518b84b580df30442cb47b82f326c9b8b
novice-is-me <reginajalandoni13@gmail.com>
2025-06-19 10:23:29 +0800

    Adding Market Page

---
+++ src/components/Button.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: "button",
+  },
+  value: {
+    type: String,
+    default: "active",
+  },
+  status: {
+    type: String,
+    default: "increase",
+  },
+});
+</script>
+
+<template>
+  <div>
+    <button
+      :type="type"
+      :class="[
+        `${
+          value === 'active'
+            ? 'bg-green-500 text-white'
+            : 'bg-transparent border border-gray-800 '
+        } px-4 py-2 rounded-lg transition duration-300 ease-in-out`,
+      ]"
+    >
+      {{ label }}
+    </button>
+  </div>
+</template>
--- src/pages/Market.vue
+++ src/pages/Market.vue
@@ -1,5 +1,81 @@
-<script setup></script>
+<script setup>
+import Button from "@/components/Button.vue";
+import CryptoCard from "@/components/CryptoCard.vue";
+</script>
 
 <template>
-  <div>Market Page</div>
+  <div class="pt-10 space-y-14">
+    <div class="space-y-4 font-[Inter]">
+      <h1 class="crypto-text-gradient text-4xl">Cryptocurrency Market</h1>
+      <p class="text-subtext">
+        Real-time prices and market data for the top cryptocurrencies
+      </p>
+    </div>
+
+    <!-- Search -->
+    <div
+      class="glass-effect p-6 rounded-lg flex items-center gap-x-4 justify-between"
+    >
+      <!-- Search Input -->
+      <div
+        class="flex items-center rounded-lg w-1/2"
+        style="border: 1px solid rgba(255, 255, 255, 0.1)"
+      >
+        <i class="fa-solid fa-magnifying-glass text-subtext p-2"></i>
+        <input
+          type="text"
+          placeholder="Search for a cryptocurrency..."
+          class="w-full p-2 focus:outline-none focus:ring-1 rounded-lg focus:ring-green-500 bg-transparent text-white"
+          style="border: 1px solid rgba(255, 255, 255, 0.1); border-left: none"
+        />
+      </div>
+      <div class="flex justify-between items-center pt-4">
+        <Button :label="'Price'" :value="'inactive'" />
+        <Button :label="'24h Change'" :value="'inactive'" />
+        <Button :label="'Market Cap'" :value="'active'" />
+      </div>
+    </div>
+
+    <!-- Result / Data -->
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-6"
+    >
+      <CryptoCard
+        crypto="Bitcoin"
+        symbol="BTC"
+        price="$27,000.00"
+        change="2.5%"
+        marketCap="$500B"
+        volume="$25B"
+        icon="fa-solid fa-bitcoin-sign"
+      />
+      <CryptoCard
+        crypto="Ethereum"
+        symbol="ETH"
+        price="$1,800.00"
+        change="-1.2%"
+        marketCap="$200B"
+        volume="$10B"
+        icon="fa-brands fa-ethereum"
+      />
+      <CryptoCard
+        crypto="Solana"
+        symbol="SOL"
+        price="$0.50"
+        change="0.5%"
+        marketCap="$25B"
+        volume="$1B"
+        icon="fa-solid fa-circle-notch"
+      />
+      <CryptoCard
+        crypto="Cardano"
+        symbol="ADA"
+        price="$100.00"
+        change="-0.3%"
+        marketCap="$10B"
+        volume="$500M"
+        icon="fa-brands fa-ethereum"
+      />
+    </div>
+  </div>
 </template>
