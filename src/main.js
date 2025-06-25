ef7a0c264bb624063686e2d5af4eb5466e8e2c2b
novice-is-me <reginajalandoni13@gmail.com>
2025-06-25 20:28:45 +0800

    Installed primevue

--- src/main.js
+++ src/main.js
@@ -7,9 +7,11 @@ import router from "./router/index.js";
 import "./assets/main.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import { createPinia } from "pinia";
+import Primevue from "primevue/config";
 
 const app = createApp(App);
 const pinia = createPinia();
 app.use(router);
 app.use(pinia);
+app.use(Primevue);
 app.mount("#app");
